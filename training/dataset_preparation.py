b70b458bbde32462f7dd1d2b3d08e95941958346
Kalp Kanungo <kalpkanungo@Kalps-MacBook-Air.local>
2026-03-16 20:33:14 +0530

    updated data_prep

--- training/dataset_preparation.py
+++ training/dataset_preparation.py
@@ -1,15 +1,16 @@
 from datasets import load_dataset
 import json
 
-dataset = load_dataset("ms_marco","v1.1")
+dataset = load_dataset("microsoft/ms_marco","v1.1")
 
 train = dataset["train"]
 
 pairs = []
 
-for i in range(50000):
+for i in range(30000):
     q = train[i]["query"]
-    pos = train[i]["passages"]["passage_text"][0]
+    passages = train[i]["passages"]["passage_text"]
+    pos = passages[0]
     pairs.append({"query":q,"passage":pos,"label":1})
 
 with open("training/msmarco_train.json","w") as f:
